d3bb479e48e52dac1e80c7be885a8a894767f807
shane <shane@hvpatriotguard.org>
2017-11-18 14:50:35 -0500

    First Commit, Repo Initiatialization

---
+++ READEME.md
@@ -0,0 +1,6 @@
+AST-DNS is a simple easy to use open source Dynamic DNS mapping software that isn't a full DNS server
+AST-DNS works by pulling mappings from the server and adding them directly to your computers host file 
+You will not affect all the devices on your network with this, only your computer.
+
+This makes it easier for friends to communicate amongst themselves in a private environment with dynamic 
+IP addresses
---
+++ server/server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python -e
+
+import sqlite3	
+import sys
+	
+def open_db(con, db_file):
+	# Opens the database file
+	con = sqlite3.connect(db_file)
+	return con
+	
+def	init_db(con):
+	# Initializes & sets up the database for the first run
+	q = "CREATE TABLE mappings (id INTEGER PRIMARY KEY, hostname, ip)"
+	con.execute(q)
+	
+	q = "CREATE UNIQUE INDEX map_host ON mappings(hostname)"
+	con.execute(q)
+	
+def map_host(con, hostname, ip):
+	# Stores a key value entry into the sqlite3 database for the host specified
+	# Will update if the hostname already exists
+	q = "SELECT * FROM mappings WHERE hostname = {0}".format(hostname)
+	results = con.execute(q)
+	results = results.fetchall()
+	if (len(results) > 0):
+		#Already Exists we need to update
+		q = "UPDATE mappings SET ip = {0} WHERE hostname = {1}".format(ip, hostname)
+	else:
+		q = "INSERT INTO mappings(hostname, ip) VALUES('{0}','{1}')".format(hostname, ip)
+	
+	res = con.execute(q)
+	if res.rowscount > 0:
+		return TRUE
+	else:
+		return FALSE
+		
+
