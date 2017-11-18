a65696b96a00d7cb47e242a621421155b0dd0055
shane <shane@hvpatriotguard.org>
2017-11-18 15:25:05 -0500

    Started the server code

--- server/server.py
+++ server/server.py
@@ -1,6 +1,15 @@
-#!/usr/bin/python -e
+#!/usr/bin/python
 
 import sys
 import ast_db
+import ast_network
 
-
+def print_usage():
+	print("Usage: {0} <db_file> <port number>\r\n".format(sys.argv[0]))
+	print("Starts the AST DNS Server using <db_file> as the database file and listens on <port number> for clients")
+	
+# Main start of program
+if len(sys.argv) < 3:
+	print_usage()
+	exit()
+	
