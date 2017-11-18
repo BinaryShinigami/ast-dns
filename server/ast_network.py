af0d43cf8ba2646c4d3f0484b0255a4f04a2895c
shane <shane@hvpatriotguard.org>
2017-11-18 15:23:55 -0500

    Started the server code

--- server/ast_network.py
+++ server/ast_network.py
@@ -1,2 +1,19 @@
 # Network functions for AST_DNS Server
 
+import socketserver
+import ast_db
+
+class ClientHandler(socketserver.BaseRequestHandler):
+	# This class handles the actual client connections once connected
+	global ast_database_lock
+	
+	def handle(self):
+		#Handle client connection here
+		pass
+	
+
+def start_server(host, port):
+	# Actually creates the TCP socket server and returns it, simply call serve_forever() to keep it going forever when ready
+	server = socketserver.TCPServer((host, port), ClientHandler)
+	return server
+	
